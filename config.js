275cedec56ce36e4650bc429e277ff1651d6a6b5
Bill Munene <billmunene937@gmail.com>
2022-09-06 03:42:17 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,22 @@
+
+
+
+var config = {
+
+  //Application Title
+  title : " Download YouTube Videos Free.",
+
+  //Error Page Title
+  errorTitle : "Page not Found (404) - GetVideo",
+
+  //Application URL
+  url : "http://example.com",
+
+  //Application Description
+  description : "Free online download YouTube videos, audio quickly in 1080p, 720p, MP4, 3GP, and more.",
+
+  //Footer text
+  
+};
+
+module.exports = config;
